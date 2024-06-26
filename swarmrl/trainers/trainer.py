98278e9a84ba136b27527fca1b7692ba579daad0
jpd-de <st177081@stud.uni-stuttgart.de>
2024-06-26 15:21:55 +0200

    Fixed doc in initialize_checkpointer.py

--- swarmrl/trainers/trainer.py
+++ swarmrl/trainers/trainer.py
@@ -73,7 +73,7 @@ class Trainer:
     def initialize_checkpointer(self, checkpoint_params: dict = None):
         """
         Initialize the checkpointer by taking the checkpoint_params key-value pairs.
-        It definitely needs the DO_CHECKPOINT key to be set to True to activate the checkpointer
+        It automatically sets the DO_CHECKPOINT key to True if the checkpoint_params is not None.
         All other keys are optional and depend on the specific checkpointing method.
         
         Currently the checkpointer can do the following:
